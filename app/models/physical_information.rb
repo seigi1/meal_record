1afed984802681c7e57fed910072c240b93a903d
seigi1 <seigi1224@gmail.com>
2022-10-25 09:44:05 +0900

    バリデーション追加

--- app/models/physical_information.rb
+++ app/models/physical_information.rb
@@ -1,4 +1,13 @@
 class PhysicalInformation < ApplicationRecord
   belongs_to :user, optional: true
-  validates :age, :body_heigth, :body_weigth ,presence: true
+  validates :age, presence: true,
+  numericality: { only_integer: true, greater_than_or_equal_to: 15, less_than_or_equal_to: 130 },
+  format: { with: /\A[0-9]+\z/ }
+  validates :body_heigth, presence: true,
+  numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 250 },
+  format: { with: /\A[0-9]+\z/ }
+  validates :body_weigth, presence: true,
+  numericality: { only_integer: true, greater_than_or_equal_to: 30, less_than_or_equal_to: 200 },
+  format: { with: /\A[0-9]+\z/ }
+  validates :basal_metabolism, numericality: { only_integer: true }
 end
